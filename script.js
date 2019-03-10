45d995168a26c9f755623ececfed2db5da603f72
VisualShock <vshogg@gmail.com>
2019-03-11 00:02:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
+/*
+    Code is very similar to Syzov Vladyslav and Viktor Panchenko work
+    Reworked slightly but still can be identified :)
+*/
+
+/*
+  Don't use such school pascal lessons style
+  It is very hard to get what is going on in your code :)
+
+  A lot of hardcoded values
+
+  Why arr is global if you use it only in the one routine
+*/
 let arr=[],game,ei,ej;
 
+/*
+    Too complicated logic for shuffling
+*/
 function shuffle(arr){
+    /*
+        You defined a bunch of variables in the loop scope
+        but these 2 in the function scope
+        for what?
+
+        Guess they also can be defined in the loop scope :)
+    */
     let j, temp;
     for (let i = 14;i>0;i--){
         j = Math.floor(Math.random() * (i));
         let k = (Math.floor(j/4));//rows
+        /* (Math.floor(j/4)) already defined as k */
         let l = j - Math.floor(j/4)*4;//columns
         let arrI = (Math.floor(i/4));
-        let arrJ = i -(Math.floor(i/4))*4;
+        /* (Math.floor(i/4)) already defined as arrI */
+        let arrJ = i - (Math.floor(i/4))*4;
         temp =  arr[arrI][arrJ];
         arr[arrI][arrJ] = arr[k][l];
         arr[k][l] = temp;
@@ -26,20 +51,39 @@ function checkSolutions(arr){
             if (arr[ik][il]>arr[k][l])inc++;
         }
     }
+    /*
+        just: return inc % 2 == 0;
+    */
     if (inc%2==0) return true;
     else return false;
 }
 
+/*
+  Use of DOM Level 2 events (addEventListener) is better practice
+*/
 window.onload = function() {
     game = document.getElementById("game");
     newGame();
 };
 
+/*
+  Code is very messy
+  You have to divide logic for more routines
+  instead of having all in one
+*/
 function clickEvent(e) {
+    /*
+        Bad idea to use element`s id as data storage
+        event.target instead of event.srcElement or just 'this' as long as you hadn't overrided context for your handler
+    */
     var el = e.srcElement || e.target;
     let i = el.id.charAt(0),
         j = el.id.charAt(2);
     console.log(el.innerHTML);
+
+    /*
+        A lot of nested constructions
+    */
     if (
         (i == ei && Math.abs(j - ej) == 1) ||
         (j == ej && Math.abs(i - ei) == 1)
@@ -49,10 +93,19 @@ function clickEvent(e) {
         ei = i;
         ej = j;
         var victory = true;
+
+        /*
+          Very strange win verification
+          Could use much more simplier
+        */
         for (let i = 0; i < 4; ++i)
             for (let j = 0; j < 4; ++j)
                 if (i + j != 6 && document.getElementById(i + " " + j).innerHTML != i * 4 + j + 1){
                     victory = false;
+                    /*
+                        This break will return only from 1 loop
+                        But once you got your flag false you have to break both
+                    */
                     break;
                 }
                 if (victory)alert("VICTORY!!");
@@ -66,6 +119,9 @@ function newGame() {
     while (checkSolutions(arr)==false) //check solutions and shuffle
         arr = shuffle(arr);
     var table = document.createElement("table"),// create board
+    /*
+        twrapper is not valid html element
+    */
         twrapper = document.createElement("twrapper");
     table.appendChild(twrapper);
     for (i = 0; i < 4; i++) {
@@ -79,6 +135,11 @@ function newGame() {
         }
         twrapper.appendChild(row);
     }
+
+    /*
+        No need for this verification
+        Just clean box content (box.innerHTML = '') and insert your table
+    */
     if (game.childNodes.length == 1) game.removeChild(game.firstChild);
     game.appendChild(table);
 }
